7237e8fbbc37057564ecafb61e0778845842cf25
WolfSlayer04 <133533095+WolfSlayer04@users.noreply.github.com>
2025-08-04 09:57:02 -0600

    Update requireAdmin.go

--- Middleware/requireAdmin.go
+++ Middleware/requireAdmin.go
@@ -7,6 +7,15 @@ import (
 // Requiere que el usuario sea admin (acceso total)
 func RequireAdmin(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // SIEMPRE agrega los headers de CORS
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Tipo-Usuario")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+
         tipoUsuario := r.Header.Get("X-Tipo-Usuario") // O donde guardes el tipo
         if tipoUsuario != "admin" {
             http.Error(w, "No tienes permisos suficientes", http.StatusForbidden)
